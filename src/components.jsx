a00d2d4168178adc30d86eec47e261cdc331bda8
0binny0 <binnyhh@protonmail.com>
2024-05-22 12:01:49 -0700

    removed aria-label from paragraph elements

--- src/components.jsx
+++ src/components.jsx
@@ -1,9 +1,9 @@
 
 function Screen({pad, sound_bank, volume}) {
     return <section className="display" aria-label="drum_machine_info">
-        <p aria-label="title_of_sound_played">Pad: {pad}</p>
-        <p aria-label="selected_sound_bank">Sound Bank: {sound_bank}</p>
-        <p aria-label="volume_level">Volume: {volume}</p>
+        <p>Pad: {pad}</p>
+        <p>Sound Bank: {sound_bank}</p>
+        <p>Volume: {volume}</p>
     </section>
 }
 
