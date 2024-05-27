52abe7b542bb5e67f5b27eb3c0b0a08ca6eab2bd
0binny0 <binnyhh@protonmail.com>
2024-05-26 21:34:13 -0700

    added handle_stop_loop function

--- src/components.jsx
+++ src/components.jsx
@@ -2,7 +2,7 @@
 import {mp3_files} from "./sound_files.js";
 import {PowerContext} from "./context.jsx";
 
-import {useContext, useEffect, useRef} from "react";
+import {useContext, useEffect, useRef, useState} from "react";
 
 
 function Screen({pad, sound_bank, volume}) {
@@ -13,8 +13,8 @@ function Screen({pad, sound_bank, volume}) {
     </section>
 }
 
-function Pad({id, src, style}) {
-    return <button id={id} className="pad" style={style}>
+function Pad({id, src, style, onMouseDown}) {
+    return <button onMouseDown={onMouseDown} id={id} className="pad" style={style}>
         <audio src={src}></audio>
     </button>
 }
@@ -23,27 +23,39 @@ function Pads({sound_bank, highlight, activeHighlight}) {
 
     const power_on = useContext(PowerContext);
     let loop = useRef(null);
-    let current_pad_ref = useRef(null);
+    let pads_ref = useRef(null);
     let counter_ref = useRef(0);
+    //nextLoop is trigger a new loop upon a user mouse event
+    let [nextLoop, setNextLoop] = useState(0);
 
     useEffect(() => {
         // loops through the pads to highlight each pad for 1 second when the drum machine is on
         if (power_on) {
             loop.current = setTimeout(() => {
-                activeHighlight(current_pad_ref.current.children[counter_ref.current].id);
+                activeHighlight(pads_ref.current.children[counter_ref.current].id);
                 ++counter_ref.current;
                 if (counter_ref.current > 8) {
                     counter_ref.current = 0;
                 }
-            }, 1000);
+            }, 800);
             return () => clearTimeout(loop.current);
         }
-    }, [power_on, highlight]);
+    }, [power_on, highlight, nextLoop]);
+
+    function handle_stop_loop(e) {
+        clearTimeout(loop.current);
+        let pad_index = [...pads_ref.current.children].findIndex(
+            (pad) => pad.id === e.currentTarget.id
+        );
+        counter_ref.current = pad_index;
+        //Math.random() is used to re-render with a new setTimeout loop
+        setNextLoop(Math.random());
+    }
 
     //mp3 files are selected based on what sound bank a user chooses
     const mp3s = sound_bank === "A" ? mp3_files[0]: mp3_files[1];
     const pads = mp3s.map((mp3_file, i) => {
-        return <Pad key={crypto.randomUUID()} id={mp3_file.id} src={mp3_file.src} style={
+        return <Pad onMouseDown={handle_stop_loop} key={crypto.randomUUID()} id={mp3_file.id} src={mp3_file.src} style={
             {
                 background: power_on ? mp3_file.style.background : "rgba(0, 0, 0, 0.1)",
                 boxShadow: power_on ? mp3_file.style.boxShadow : "inset 0 0 7px 0 rgba(0, 0, 0, 0.6)",
@@ -51,7 +63,7 @@ function Pads({sound_bank, highlight, activeHighlight}) {
             }
         } />
     });
-    return <div ref={current_pad_ref} id="pad_interface" className="pads_wrapper">{pads}</div>
+    return <div ref={pads_ref} id="pad_interface" className="pads_wrapper">{pads}</div>
 }
 
 export {Screen, Pad, Pads};
