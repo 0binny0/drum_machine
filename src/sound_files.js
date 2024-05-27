97b3502d6fdb360755edf854d70065720043ded9
0binny0 <binnyhh@protonmail.com>
2024-05-26 20:00:45 -0700

    changed styles from an array to a single object

--- src/sound_files.js
+++ src/sound_files.js
@@ -1,25 +1,31 @@
 
+const style = {
+        background: "rgb(225, 255, 240)",
+        opacity: "0.5",
+        boxShadow: "inset 0px 0px 6px 2px darkgrey"
+};
+
 const mp3_files = [
     [
-        {id: "pad_Heater_1", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"},
-        {id: "pad_Heater_2", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"},
-        {id: "pad_Heater_3", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"},
-        {id: "pad_Heater_4", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"},
-        {id: "pad_Clap", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"},
-        {id: "pad_Open_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"},
-        {id: "pad_Kick_n'_Hat", src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"},
-        {id: "pad_Kick", src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"},
-        {id: "pad_Closed_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"}
+        {id: "pad_Heater_1", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3", style: style},
+        {id: "pad_Heater_2", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3", style: style},
+        {id: "pad_Heater_3", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3", style: style},
+        {id: "pad_Heater_4", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3", style: style},
+        {id: "pad_Clap", src: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3", style: style},
+        {id: "pad_Open_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3", style: style},
+        {id: "pad_Kick_n'_Hat", src: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3", style: style},
+        {id: "pad_Kick", src: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3", style: style},
+        {id: "pad_Closed_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3", style: style}
     ],[
-        {id: "pad_Chord_1", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3"},
-        {id: "pad_Chord_2", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3"},
-        {id: "pad_Chord_3", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3"},
-        {id: "pad_Shaker", src: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3"},
-        {id: "pad_Open_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3"},
-        {id: "pad_Closed_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3"},
-        {id: "pad_Punchy-Kick", src: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3"},
-        {id: "pad_Side_Stick", src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3"},
-        {id: "pad_Snare", src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3"}
+        {id: "pad_Chord_1", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_1.mp3", style: style},
+        {id: "pad_Chord_2", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_2.mp3", style: style},
+        {id: "pad_Chord_3", src: "https://s3.amazonaws.com/freecodecamp/drums/Chord_3.mp3", style: style},
+        {id: "pad_Shaker", src: "https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3", style: style},
+        {id: "pad_Open_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Dry_Ohh.mp3", style: style},
+        {id: "pad_Closed_HH", src: "https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3", style: style},
+        {id: "pad_Punchy-Kick", src: "https://s3.amazonaws.com/freecodecamp/drums/punchy_kick_1.mp3", style: style},
+        {id: "pad_Side_Stick", src: "https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3", style: style},
+        {id: "pad_Snare", src: "https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3", style: style}
     ]
 ];
 
