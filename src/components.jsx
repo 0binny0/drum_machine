0c586e2de8fafb5c00ad478e86e83219c72aa087
0binny0 <binnyhh@protonmail.com>
2024-05-26 19:55:09 -0700

    added useEffect to <Pads /> component

--- src/components.jsx
+++ src/components.jsx
@@ -2,7 +2,7 @@
 import {mp3_files} from "./sound_files.js";
 import {PowerContext} from "./context.jsx";
 
-import {useContext} from "react";
+import {useContext, useEffect, useRef} from "react";
 
 
 function Screen({pad, sound_bank, volume}) {
@@ -14,23 +14,44 @@ function Screen({pad, sound_bank, volume}) {
 }
 
 function Pad({id, src, style}) {
-    return <button className="pad" style={style}>
-        <audio id={id} src={src}></audio>
+    return <button id={id} className="pad" style={style}>
+        <audio src={src}></audio>
     </button>
 }
 
-function Pads({sound_bank}) {
+function Pads({sound_bank, highlight, activeHighlight}) {
+
     const power_on = useContext(PowerContext);
+    let loop = useRef(null);
+    let current_pad_ref = useRef(null);
+    let counter_ref = useRef(0);
+
+    useEffect(() => {
+        // loops through the pads to highlight each pad for 1 second when the drum machine is on
+        if (power_on) {
+            loop.current = setTimeout(() => {
+                activeHighlight(current_pad_ref.current.children[counter_ref.current].id);
+                ++counter_ref.current;
+                if (counter_ref.current > 8) {
+                    counter_ref.current = 0;
+                }
+            }, 1000);
+            return () => clearTimeout(loop.current);
+        }
+    }, [power_on, highlight]);
+
+    //mp3 files are selected based on what sound bank a user chooses
     const mp3s = sound_bank === "A" ? mp3_files[0]: mp3_files[1];
-    const pads = mp3s.map((mp3_file) => {
+    const pads = mp3s.map((mp3_file, i) => {
         return <Pad key={crypto.randomUUID()} id={mp3_file.id} src={mp3_file.src} style={
             {
                 background: power_on ? mp3_file.style.background : "rgba(0, 0, 0, 0.1)",
-                boxShadow: power_on ? mp3_file.style.boxShadow : "inset 0 0 7px 0 rgba(0, 0, 0, 0.6)"
+                boxShadow: power_on ? mp3_file.style.boxShadow : "inset 0 0 7px 0 rgba(0, 0, 0, 0.6)",
+                opacity: power_on && highlight !== mp3_file.id ? mp3_file.style.opacity : "0.9"
             }
         } />
     });
-    return <div className="pads_wrapper">{pads}</div>
+    return <div ref={current_pad_ref} id="pad_interface" className="pads_wrapper">{pads}</div>
 }
 
 export {Screen, Pad, Pads};
